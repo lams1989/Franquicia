f165770a622498ac5c2afd6d207d4eb328e74cb4
luis.munoz <luis.munoz@ceiba.com.co>
2025-04-08 10:27:51 -0500

    Se ajusta despliegue terraform

--- terraform/modules/iam_roles/roles.tf
+++ terraform/modules/iam_roles/roles.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 data "aws_iam_policy_document" "ecs_task_assume_role" {
